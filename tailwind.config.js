c3d54fd04571a6ff340f32c3c54af3fd9ca0be17
chencheng0924 <abs255667@gmail.com>
2024-02-21 22:13:20 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "main-red": "#942023",
+        "hover-text": "#8B0000",
+        "text-white": "#FCF8EB",
+        "btn-bg": "#8B0000",
+        "sec-btn-bg": "#D0665F",
+        "bgc-white": "#FBF8EB"
+      },
+      screens: {
+        'tablet': { 'max': '999px' },
+        // 'laptop': { 'min': '950px', 'max': '1130px' },
+        'desktop': { 'min': '1000px' },
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
